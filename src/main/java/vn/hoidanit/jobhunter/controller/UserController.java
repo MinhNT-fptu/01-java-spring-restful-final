aff9a5b761f60b9eff9783594ae901f7dd775a9c
MinhNT-fptu <ntminh1302@gmail.com>
2026-01-27 15:10:14 +0700

    add function finduserbyid and updateuser

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,5 +1,8 @@
 package vn.hoidanit.jobhunter.controller;
 
+import java.util.Optional;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +12,8 @@ import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
 
 //@Controller → thường dùng cho web MVC + view (JSP/HTML)
 @RestController // dùng cho API trả JSON/text
@@ -39,4 +44,25 @@ public class UserController {
 
         return "delete success";
     }
+
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable("id") long id) {
+
+        // Optional<User> user = this.userService.handleGetUserById(id);
+        User user = this.userService.fetchUserById(id);
+
+        return user;
+    }
+
+    @GetMapping("/user")
+    public List<User> getAllUsers() { // lấy tất cả user java tự convert List<User> thành JSON array
+        return this.userService.fetchAllUsers();
+    }
+
+    @PutMapping("/user")
+    public User updateUserById(@RequestBody User updateUser) {
+        User updatedUser = this.userService.handleUpdateUser(updateUser);
+        return updatedUser;
+    }
+
 }
--- src/main/java/vn/hoidanit/jobhunter/service/UserService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserService.java
@@ -3,6 +3,9 @@ package vn.hoidanit.jobhunter.service;
 import vn.hoidanit.jobhunter.domain.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 import vn.hoidanit.jobhunter.repository.UserRepository;
 
 @Service // khai báo đây là tầng Service(xử lý logic) -> tạo bean để DI
@@ -26,4 +29,26 @@ public class UserService {
         this.userRepository.deleteById(id);
     }
 
+    // xử lý logic lấy user theo id
+    public User fetchUserById(long id) {
+        return this.userRepository.findById(id).orElse(null);
+    }
+
+    // xử lý logic lấy tất cả user
+    public List<User> fetchAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    // xử lý logic update user
+    public User handleUpdateUser(User updateUser) {
+        User user = this.fetchUserById(updateUser.getId());
+        if (user != null) {
+            user.setName(updateUser.getName());
+            user.setEmail(updateUser.getEmail());
+            user.setPassword(updateUser.getPassword());
+            // update user trong DB
+            this.userRepository.save(user); // create hay update phải save thì DB mới thay đổi
+        }
+        return user;
+    }
 }
