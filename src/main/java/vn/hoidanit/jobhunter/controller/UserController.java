a3858affffd72344f17b7a9befec29de2faba463
MinhNT-fptu <ntminh1302@gmail.com>
2026-01-30 17:52:07 +0700

    GlobalException by @RestControllerAdvice

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import io.micrometer.core.ipc.http.HttpSender.Response;
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
+import vn.hoidanit.jobhunter.service.error.IdInvalidException;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -46,8 +49,14 @@ public class UserController {
     @DeleteMapping("/users/{id}") // truyền động qua path
 
     // nếu không muốn trả dữ liệu cho generic thành void
-    public ResponseEntity<Void> deleteUserById(@PathVariable("id") long id) { // map path variable vào tham số id
-
+    public ResponseEntity<Void> deleteUserById(@PathVariable("id") long id) throws IdInvalidException {
+        // map path
+        // variable vào
+        // tham số
+        // id
+        if (id >= 1500) {
+            throw new IdInvalidException("Id khong duoc lon hon 1500!"); // trả ra exception ghi trong terminal
+        }
         this.userService.handleDeleteUserById(id);
 
         // return ResponseEntity.ok("Delete user successfully"); // cách viết ngắn hơn
